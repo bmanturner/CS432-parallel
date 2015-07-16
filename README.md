0423bd7344cc9eb477585ea3c7d1a24c47aadcbd
bfturner <brendanfturner@gmail.com>
2015-07-16 13:15:36 -0500

    add mpi solution; rename files; update readme

--- README.md
+++ README.md
@@ -1,10 +1,42 @@
 # CS432
 UAB CS432 - Parallel Programming
 
-CS432 covers Posix Threads and Distributed Computing (MPI). 
+CS432 covers Posix Threads and Distributed Computing (via MPI).
 
-## mp_counter.py
+## mp_counter.py (Multiprocessing 'Hello World')
 The 'hello world' of multiprocessing ported from an example C file discussed during class. Features the creation of multiple (4) threads that simultaneously run a method that counts to a specified integer. The results are then combined and stored under a global counter variable in shared memory. Also includes the use of a lock that prevents multiple threads from modifying the counter simultaneously.
 
-## mp_area.py
+# The Assignment
+Determine the area of the region created by four overlapping circles of radius 3 centered at (-2,0), (2,0), (0,-2), and (0,2).
+
+## The Solution
+Because the region is symmetrical across both the X and Y axis, I chose to find the area of the region constrained to the first quadrant (0 <= X <= 1 and 0 <= Y <= 1) and then multiply it by four to determine the total area.
+
+
+
+### mp_monte_carlo.py (Multiprocessing Monte Carlo Simulation)
 Uses multiple processes to determine the area of an arbitrarily defined 2D region by means of generating and testing (x,y) coordinate pairs. An observer method intermittently tests the results until a satisfactory degree of accuracy can be obtained. Features lock-less multiprocessing by assigning each process unique indexes allowing for the modification of an array in shared memory without conflict.
+
+#### Requirements
+* Python 2.7.x
+* [NumPy](http://www.numpy.org/)
+
+#### Running
+```
+% python mp_monte_carlo.py N
+[where N is an integer >= 1]
+```
+
+
+### mpi_monte_carlo.c (Message Passing Interface Monte Carlo Simulation)
+Uses multiple hosts to determine the area of an arbitrarily defined 2D region by means of generating and testing (x,y) coordinate pairs. The master process intermittently tests the results until a satisfactory degree of accuracy can be obtained. Features asynchronous communication by means of MPI_Iprobe(), a non-blocking method that determines whether a message is waiting to be received. As a result, slower processes to not impede the performance of faster processes.
+
+#### Requirements
+* [MPICH](http://www.mpich.org/)
+
+#### Running
+```
+% mpicc mpi_monte_carlo.c -o mpi_monte_carlo
+% mpirun -np X mpi_monte_carlo
+[where N is an integer >= 2]
+```
---
+++ mpi_monte_carlo.c
@@ -0,0 +1,93 @@
+/*****************************************************************************/
+/**                                                                         **/
+/**     NAME:            Brendan Turner                                     **/
+/**     PROJECT:         Assignment #2 - MPI Monte Carlo                    **/
+/**     DATE:            07/16/2015                                         **/
+/**                                                                         **/
+/*****************************************************************************/
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <mpi.h>
+
+
+int main(int argc, char **argv) {
+    int rank, size, root, hitIndex, totalIndex, msgWaiting;
+    double results[2];
+    hitIndex = 0;
+    totalIndex = 1;
+    root = 0;
+
+    MPI_Init(&argc, &argv);
+    MPI_Comm comm = MPI_COMM_WORLD;
+    MPI_Comm_rank(comm, &rank);
+    MPI_Comm_size(comm, &size);
+    MPI_Status status;
+    MPI_Request request;
+
+    // is master process
+    if(rank == root) {
+        double totalHits, totalPokes, area;
+        int i = 1;
+
+        while (1) {
+            sleep(1);
+            // check each slave process for results (non-blocking)
+            for (i = 1; i < size; i++) {
+                MPI_Iprobe(i, 0, comm, &msgWaiting, &status);
+                // if slave process is sending results
+                if (msgWaiting) {
+                    MPI_Recv(&results, 2, MPI_DOUBLE, i, 0, comm, &status);
+                    totalHits += results[hitIndex];
+                    totalPokes += results[totalIndex];
+                }
+            }
+            if (totalPokes >= 25000000000) {
+                area = (totalHits / totalPokes) * 4;
+                printf("Area=%.12lf\n", area);
+                // send terminating message to each slave process
+                for (i = 1; i < size; i++) {
+                    MPI_Isend(&area, 1, MPI_DOUBLE, i, 0, comm, &request);
+                }
+                break;
+            }
+        }
+    // is slave process
+    } else {
+        double x, y, dx, dy;
+        int i = 0;
+
+        // seed random gen with process specific seed
+        srand(rank * 999999999);
+
+        while (1) {
+            if (results[totalIndex] < 50000000) {
+                x = ((double)rand()/(double)RAND_MAX);
+                y = ((double)rand()/(double)RAND_MAX);
+                dx = x + 2;
+                dy = y + 2;
+                // fast, basic region check
+                if (dx + y <= 3 && x + dy <=3) {
+                    results[hitIndex] += 1;
+                // slow, precise region check
+                } else if (dx*dx + y*y <= 9 && x*x + dy*dy <= 9) {
+                    results[hitIndex] += 1;
+                }
+                results[totalIndex] += 1;
+            } else {
+                // check for terminating message
+                MPI_Iprobe(root, 0, comm, &msgWaiting, &status);
+                if (msgWaiting) {
+                    break;
+                }
+                // send round of results to master process
+                MPI_Send(&results, 2, MPI_DOUBLE, root, 0, comm);
+                results[hitIndex] = 0;
+                results[totalIndex] = 0;
+            }
+        }
+    }
+
+    MPI_Finalize();
+}
