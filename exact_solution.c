b0d557796ae9004c7f517e988e14ee5a3faf2a92
bfturner <brendanfturner@gmail.com>
2015-07-21 10:35:53 -0500

    formatting; comments

--- exact_solution.c
+++ exact_solution.c
@@ -6,20 +6,23 @@ int main(int argc, char **argv) {
   long n = 1000000000;
   int i;
 
-
-
+  // find point where circles intersect
   s = sqrtl(3.5L) - 1;
+
+  // width of rectangle
   h = s / n;
 
-  printf("%.60Le\n", h);
-
-  area = ((sqrtl(9) - 2 - s) + (sqrtl(9-(s*s)) - 2 - s))/2;
+  // sum area of each rectangle
   for (i = 1; i < n; i++) {
     x = i * h;
     area += (sqrtl(9-(x*x)) - 2 - s);
   }
 
+  // double area to account for symmetry in first quadrant
   area *= h*2;
+  // add area of square between origin and circle intersection
   area += (s*s);
-  printf("Area=%.15Le\n", area * 4);
+
+  // multiply area by 4 to account for radial symmetry around origin
+  printf("Area=%.14Le\n", area * 4);
 }
