99a21facd47f72e7337c4ac47d52b4adb3e61a4d
bfturner <brendanfturner@gmail.com>
2015-07-27 11:33:26 -0500

    initialized variables

--- mpi_monte_carlo.c
+++ mpi_monte_carlo.c
@@ -13,11 +13,12 @@
 
 
 int main(int argc, char **argv) {
-    int rank, size, root, hitIndex, totalIndex, msgWaiting;
-    double results[2];
-    hitIndex = 0;
-    totalIndex = 1;
-    root = 0;
+    int rank, size, i;
+    int root = 0;
+    int hits = 0;
+    int total = 0;
+    int msg_waiting = 0;
+    double results[2] = {0};
 
     MPI_Init(&argc, &argv);
     MPI_Comm comm = MPI_COMM_WORLD;
@@ -28,18 +29,19 @@ int main(int argc, char **argv) {
 
     // is master process
     if(rank == root) {
-        double totalHits, totalPokes, area;
-        int i = 1;
+        double totalHits = 0;
+        double totalPokes = 0;
+        double area;
 
         while (1) {
             // check each slave process for results (non-blocking)
             for (i = 1; i < size; i++) {
-                MPI_Iprobe(i, 0, comm, &msgWaiting, &status);
+                MPI_Iprobe(i, 0, comm, &msg_waiting, &status);
                 // if slave process is sending results
-                if (msgWaiting) {
+                if (msg_waiting) {
                     MPI_Recv(&results, 2, MPI_DOUBLE, i, 0, comm, &status);
-                    totalHits += results[hitIndex];
-                    totalPokes += results[totalIndex];
+                    totalHits += results[hits];
+                    totalPokes += results[total];
                 }
             }
             if (totalPokes >= 25000000000) {
@@ -55,33 +57,32 @@ int main(int argc, char **argv) {
     // is slave process
     } else {
         double x, y, dx, dy;
-        int i = 0;
 
         // seed random gen with process specific seed
         srand(rank * 999999999);
 
         while (1) {
-            if (results[totalIndex] < 50000000) {
+            if (results[total] < 50000000) {
                 x = ((double)rand()/(double)RAND_MAX);
                 y = ((double)rand()/(double)RAND_MAX);
                 dx = x + 2;
                 dy = y + 2;
                 // fast, basic region check
                 if (dx + y <= 3 && x + dy <=3) {
-                    results[hitIndex] += 1;
+                    results[hits] += 1;
                 // slow, precise region check
                 } else if (dx*dx + y*y <= 9 && x*x + dy*dy <= 9) {
-                    results[hitIndex] += 1;
+                    results[hits] += 1;
                 }
-                results[totalIndex] += 1;
+                results[total] += 1;
             } else {
                 // check for terminating message
-                MPI_Iprobe(root, 0, comm, &msgWaiting, &status);
-                if (msgWaiting) {break;}
+                MPI_Iprobe(root, 0, comm, &msg_waiting, &status);
+                if (msg_waiting) {break;}
                 // send round of results to master process
                 MPI_Send(&results, 2, MPI_DOUBLE, root, 0, comm);
-                results[hitIndex] = 0;
-                results[totalIndex] = 0;
+                results[hits] = 0;
+                results[total] = 0;
             }
         }
     }
